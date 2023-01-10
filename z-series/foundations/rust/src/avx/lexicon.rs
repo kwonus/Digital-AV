868b251e4fd50cba67b1cee497c7f4963e06d641
Kevin Wonus <kevin@wonus.com>
2023-01-10 00:01:20 -0500

    Test Rust lexicon for variations between Z14 and Z31

--- z-series/foundations/rust/src/avx/lexicon.rs
+++ z-series/foundations/rust/src/avx/lexicon.rs
@@ -10,8 +10,8 @@ static AVXLexicon_SDK_ZEdition    :u16 = 23107;
 
 static AVXLexicon_File: &'static str = "AV-Lexicon.dxi";
 static AVXLexicon_RecordLen   :usize =        0;
-static AVXLexicon_RecordCnt   :usize =    12567;
-static AVXLexicon_FileLen     :usize =   246249;
+static AVXLexicon_RecordCnt   :usize =    12568;
+static AVXLexicon_FileLen     :usize =   246260;
 // < < < Generated-Code -- Metadata < < < //
 
 // Entities: uint16
@@ -35,8 +35,8 @@ struct AVXLexItem {
 }
 
 // > > > Generated-Code -- Initialization > > > //
-static lexicon: [AVXLexItem; 12568] = [
-	AVXLexItem { entities: 0xFFFF, search: "",  display: "", modern: "", pos: [ 12567, 31, 9 ] },
+static lexicon: [AVXLexItem; 12569] = [
+	AVXLexItem { entities: 0xFFFF, search: "",  display: "", modern: "", pos: [ 12568, 31, 9 ] },
 	AVXLexItem { entities: 0x0000, search: "a", display: "", modern: "", pos: [ 0x00000036, 0x00000094, 0x80004206, ]  },
 	AVXLexItem { entities: 0x0000, search: "i", display: "", modern: "", pos: [ 0x01074F9C, ]  },
 	AVXLexItem { entities: 0x0000, search: "o", display: "", modern: "oh", pos: [ 0x000002A8, ]  },
